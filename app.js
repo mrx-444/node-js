25ce77779a99e8d11cf8806ffd310054c50efa63
Ban Cortez <bancortez2@gmail.com>
2021-12-24 01:00:24 +0800

    fourth-commit: error-fix on listening port 5000

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const http = require("http");
 
-http
-  .createServer((req, res) => {
-    if (req.url === "/") {
-      res.write("Home Page");
-      res.end();
-    }
-
-    if (req.url === "/contact") {
-      res.write("Contact Page");
-      res.end();
-    }
-
-    if (req.url === "/about") {
-      res.write("About Page");
-      res.end();
-    }
-
-    res.write(`<h1>Oops!</h1>
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    return res.end("Home Page");
+  } else if (req.url === "/contact") {
+    return res.end("Contact Page");
+  } else if (req.url === "/about") {
+    return res.end("About Page");
+  } else {
+    return res.end(`<h1>Oops!</h1>
     <p>We can't seem to find the page you are looking for</p>
     <a href="/">Home</a>`);
-    res.end();
-  })
-  .listen(5000);
+  }
+});
+
+server.listen(5000);
